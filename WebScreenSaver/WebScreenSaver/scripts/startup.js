d18805d111d6aae0e49b547dc147337ffa5bf69c
Jonayet Hossain <jonayet.bu@gmail.com>
2015-10-10 15:19:58 +0600

    updated

--- WebScreenSaver/WebScreenSaver/scripts/startup.js
+++ WebScreenSaver/WebScreenSaver/scripts/startup.js
@@ -1,4 +1,24 @@
 ﻿(function () {
+    var _CaptionTransitions = [];
+    _CaptionTransitions["L"] = { $Duration: 900, x: 0.6, $Easing: { $Left: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+    _CaptionTransitions["R"] = { $Duration: 900, x: -0.6, $Easing: { $Left: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+    _CaptionTransitions["T"] = { $Duration: 900, y: 0.6, $Easing: { $Top: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+    _CaptionTransitions["B"] = { $Duration: 900, y: -0.6, $Easing: { $Top: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
+    var options = {
+        $AutoPlay: true,
+        $Idle: 2000,
+        $PauseOnHover: 0,
+        $CaptionSliderOptions: {
+            $Class: $JssorCaptionSlider$,
+            $CaptionTransitions: _CaptionTransitions,
+            $PlayInMode: 1,
+            $PlayOutMode: 3
+        }
+    };
+    var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+    jssor_slider1.$Pause();
+    $JssorPlayer$.$FetchPlayers(document.body);
+
     function getClockTime() {
         return moment().format('h:mm:ss A') + "<br>" + moment().format('Do MMMM YYYY');
     }
@@ -14,40 +34,31 @@
         clearInterval(clockIntervalId);
     }
 
-    function startScreenSaver() {        
-        $.blockUI({
-            fadeIn: 1000,
-            constrainTabKey: true,
-            ignoreIfBlocked: true,
-            bindEvents: true,
-            focusInput: true,
-            baseZ: 5,
-            fadeOut: 1000,
-            message: '<div> <div id="screen-clock">' + getClockTime() + '</div></div>',
-            centerY: 0,
-            css: {
-                position: 'fixed',
-                width: '100%',
-                height: '100%',
-                top: '0px',
-                left: '0px',
-                cursor: '',
-                border: 'none',
-                backgroundColor: 'gainsboro',
-                opacity: 0.6,
-                zIndex: 6
-            },
-            overlayCSS: {
-                opacity: 0,
-            },
-            onOverlayClick: $.unblockUI,
-            onBlock: startClockInterval,
-            onUnblock: stopClockInterval
-        });
+    function startScreenSaver() {
+        $('#screen-cover').css('visibility', 'visible');
+        $('#screen-cover').css('opacity', 0.6);
+
+        $('#slider1_container').css('visibility', 'visible');
+        $('#slider1_container').css('opacity', 1);
+
+        $('#screen-clock').css('visibility', 'visible');
+        $('#screen-clock').css('opacity', 1);
+        $('#screen-clock').html(getClockTime());
+        startClockInterval();
+        jssor_slider1.$Play();
     }
 
     function stopScreenSaver() {
-        $.unblockUI();
+        stopClockInterval();
+        jssor_slider1.$Pause();
+        $('#screen-cover').css('opacity', 0);
+        $('#screen-cover').css('visibility', 'hidden');
+
+        $('#slider1_container').css('visibility', 'hidden');
+        $('#slider1_container').css('opacity', 0);
+
+        $('#screen-clock').css('visibility', 'hidden');
+        $('#screen-clock').css('opacity', 0);
     }
 
     $(function () {
