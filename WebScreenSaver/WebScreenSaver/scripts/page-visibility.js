ce5e9f156e141d501a6df42208f204a1c9edc243
Jonayet Hossain <jonayet.bu@gmail.com>
2015-10-11 01:00:56 +0600

    updated

---
+++ WebScreenSaver/WebScreenSaver/scripts/page-visibility.js
@@ -0,0 +1,67 @@
+﻿(function () {
+    "use_strict";
+
+    var onVisibleEventSubscribers = [];
+    var onHiddenEventSubscribers = [];
+    var hidden = "hidden";
+
+    window.pageVisibility = {};
+    window.pageVisibility.onVisible = function (callback) {
+        onVisibleEventSubscribers.push(callback);
+    }
+
+    window.pageVisibility.onHidden = function (callback) {
+        onHiddenEventSubscribers.push(callback);
+    }
+
+    function onchange(evt) {
+        var visibility;
+        var v = "visible", h = "hidden";
+        var evtMap = {
+            focus: v,
+            focusin: v,
+            pageshow: v,
+            blur: h,
+            focusout: h,
+            pagehide: h
+        };
+
+        evt = evt || window.event;
+        if (evt.type in evtMap)
+            visibility = evtMap[evt.type];
+        else
+            visibility = this[hidden] ? h : v;
+
+        if (visibility === v) {
+            onVisibleEventSubscribers.forEach(function (subscriber) {
+                subscriber();
+            });
+        } else if (visibility === h) {
+            onHiddenEventSubscribers.forEach(function (subscriber) {
+                subscriber();
+            });
+        }
+    }
+
+    // ReSharper disable Html.EventNotResolved
+    // Standards:
+    if (hidden in document)
+        document.addEventListener("visibilitychange", onchange);
+    else if ((hidden = "mozHidden") in document)
+        document.addEventListener("mozvisibilitychange", onchange);
+    else if ((hidden = "webkitHidden") in document)
+        document.addEventListener("webkitvisibilitychange", onchange);
+    else if ((hidden = "msHidden") in document)
+        document.addEventListener("msvisibilitychange", onchange);
+        // IE 9 and lower:
+    else if ("onfocusin" in document)
+        document.onfocusin = document.onfocusout = onchange;
+        // All others:
+    else
+        window.onpageshow = window.onpagehide
+        = window.onfocus = window.onblur = onchange;
+
+    // set the initial state (but only if browser supports the Page Visibility API)
+    if (document[hidden] !== undefined)
+        onchange({ type: document[hidden] ? "blur" : "focus" });
+})();
--- WebScreenSaver/WebScreenSaver/scripts/startup.js
+++ WebScreenSaver/WebScreenSaver/scripts/startup.js
@@ -1,87 +1,94 @@
 ﻿(function () {
-    var _CaptionTransitions = [];
-    _CaptionTransitions["L"] = { $Duration: 900, x: 0.6, $Easing: { $Left: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
-    _CaptionTransitions["R"] = { $Duration: 900, x: -0.6, $Easing: { $Left: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
-    _CaptionTransitions["T"] = { $Duration: 900, y: 0.6, $Easing: { $Top: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
-    _CaptionTransitions["B"] = { $Duration: 900, y: -0.6, $Easing: { $Top: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
-    var options = {
-        $AutoPlay: true,
-        $Idle: 3000,
-        $PauseOnHover: 0,
-        $CaptionSliderOptions: {
-            $Class: $JssorCaptionSlider$,
-            $CaptionTransitions: _CaptionTransitions,
-            $PlayInMode: 1,
-            $PlayOutMode: 3
-        }
-    };
-    var jssor_slider1 = new $JssorSlider$('slider1_container', options);
-    jssor_slider1.$Pause();
-    $JssorPlayer$.$FetchPlayers(document.body);
-
-    jssor_slider1.$On($JssorSlider$.$EVT_STATE_CHANGE, stateChangeEventHandler);
-    function stateChangeEventHandler(slideIndex, progress, progressBegin, idleBegin, idleEnd, progressEnd)
-    {
-        if (slideIndex == 3 && progress == idleBegin) {
-            jssor_slider1.$Pause();
-            ytplayer.$Play();
-            setTimeout(function () {
-                ytplayer.$Quit();
-                jssor_slider1.$Play();
-            }, 10000);
-        }
-    }
-
-    function getClockTime() {
-        return moment().format('h:mm:ss A') + "<br>" + moment().format('Do MMMM YYYY');
-    }
-
-    var clockIntervalId;
-    function startClockInterval() {
-        clockIntervalId = setInterval(function () {
-            $('#screen-clock').html(getClockTime());
-        }, 500);
-    }
-
-    function stopClockInterval() {
-        clearInterval(clockIntervalId);
-    }
-
-    function startScreenSaver() {
-        $('#screen-cover').css('visibility', 'visible');
-        $('#screen-cover').css('opacity', 0.6);
-
-        $('#slider1_container').css('visibility', 'visible');
-        $('#slider1_container').css('opacity', 1);
-
-        $('#screen-clock').css('visibility', 'visible');
-        $('#screen-clock').css('opacity', 1);
-        $('#screen-clock').html(getClockTime());
-        startClockInterval();
-        jssor_slider1.$Play();
-    }
-
-    function stopScreenSaver() {
-        stopClockInterval();
-        ytplayer.$Quit();
-        jssor_slider1.$Pause();
-        $('#screen-cover').css('opacity', 0);
-        $('#screen-cover').css('visibility', 'hidden');
-
-        $('#slider1_container').css('visibility', 'hidden');
-        $('#slider1_container').css('opacity', 0);
-
-        $('#screen-clock').css('visibility', 'hidden');
-        $('#screen-clock').css('opacity', 0);
-    }
-
     $(function () {
-        $.idleTimer(10000);
-        $(document).on('idle.idleTimer', function (e, elem, obj) {
-            e.stopPropagation();
-            startScreenSaver();            
+        var inactiveTime = 10000;
+        var slideDelay = 3000;
+        var videoSlideIndex = 3;
+
+        var options = {
+            $AutoPlay: true,
+            $PauseOnHover: 0,
+            $Idle: slideDelay
+        };
+        var jssorSlider = new $JssorSlider$('jssor-slider', options);
+        jssorSlider.$Pause();
+
+        var videoPlayer = videojs("video-player");
+        function playVideo(endCallback) {
+            videoPlayer.play();
+            videoPlayer.on("ended", function () {
+                if (endCallback)
+                    endCallback();
+            });
+        }
+
+        function stopVideo() {
+            videoPlayer.pause();
+            videoPlayer.currentTime(0);
+        }
+
+        function stateChangeEventHandler(slideIndex, progress, progressBegin, idleBegin) {
+            if (slideIndex === videoSlideIndex && progress === idleBegin) {
+                jssorSlider.$Pause();
+                playVideo(function() {
+                    jssorSlider.$Play();
+                });
+            }
+        }
+        jssorSlider.$On($JssorSlider$.$EVT_STATE_CHANGE, stateChangeEventHandler);
+
+        function getClockTime() {
+            return moment().format('h:mm:ss A') + "<br>" + moment().format('Do MMMM YYYY');
+        }
+
+        var clockIntervalId;
+        function startClockInterval() {
+            clockIntervalId = setInterval(function() {
+                $('#screen-clock').html(getClockTime());
+            }, 500);
+        }
+
+        function stopClockInterval() {
+            clearInterval(clockIntervalId);
+        }
+
+        function showElement(id) {
+            $(id).css('visibility', 'visible');
+            $(id).css('opacity', 1);
+        }
+
+        function hideElement(id) {
+            $(id).css('visibility', 'hidden');
+            $(id).css('opacity', 0);
+        }
+
+        function startScreenSaver() {
+            showElement('#screen-cover');
+            showElement('#jssor-slider');
+            showElement('#screen-clock');
+            $('#screen-clock').html(getClockTime());
+            startClockInterval();
+            jssorSlider.$Play();
+        }
+
+        function stopScreenSaver() {
+            stopVideo();
+            stopClockInterval();
+            jssorSlider.$Pause();
+            hideElement('#screen-cover');
+            hideElement('#jssor-slider');
+            hideElement('#screen-clock');
+        }
+
+        $.idleTimer(inactiveTime);
+        $(document).on('idle.idleTimer', function() {
+            startScreenSaver();
         });
-        $(document).on('active.idleTimer', function (e, elem, obj, triggerevent) {
+
+        $(document).on('active.idleTimer', function() {
+            stopScreenSaver();
+        });
+
+        pageVisibility.onHidden(function() {
             stopScreenSaver();
         });
     });
